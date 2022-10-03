366b5482407ab0b647cdb83a1682c8adbdb6e8a5
mangiu90 <giudicimanuel@gmail.com>
2022-10-02 21:59:56 -0300

    tarea envio resumen

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,10 @@
 
 namespace App\Console;
 
+use App\Mail\Resumen;
+use App\Models\Evento;
+use App\Models\Movimiento;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +19,39 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            $headerData = ['USUARIO', 'EVENTO', 'FECHA', 'MONTO', 'TIPO', 'DETALLE'];
+            $arrayData = [];
+            Movimiento::with(['user', 'evento'])->each(function ($mov) use (&$arrayData) {
+                $arrayData[] = [
+                    $mov->user->name,
+                    $mov->evento->nombre,
+                    $mov->fecha,
+                    $mov->monto,
+                    $mov->tipo,
+                    $mov->detalle,
+                ];
+            });
+    
+            $eventos = [];
+    
+            Evento::all()->map(function ($evento) use (&$eventos) {
+                $ev['nombre'] = $evento->nombre;
+                $ev['gastos'] = number_format((float)$evento->gastos(), 2);
+                $ev['gastos_usuario'] = number_format((float)$evento->gastosPorUsuario(), 2);
+                $ev['users'] = $evento->users->transform(function ($user) use ($evento) {
+                    return [
+                        'name' => $user->name,
+                        'gastos' => number_format((float)$user->gastosPorEvento($evento->id), 2),
+                        'balance' => number_format((float)$user->balancePorEvento($evento->id), 2),
+                    ];
+                });
+    
+                $eventos[] = $ev;
+            });
+    
+            Mail::to('giudicimanuel@gmail.com')->send(new Resumen($eventos, $headerData, $arrayData));
+        })->twiceDaily(1, 13);
     }
 
     /**
