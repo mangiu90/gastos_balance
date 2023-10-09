d92764e2de30f35f513f65db05928131d844ab97
manuel <giudicimanuel@gmail.com>
2023-10-09 19:27:07 -0300

    test email

---
+++ resources/views/emails/test.blade.php
@@ -0,0 +1,3 @@
+{{-- a test mail template --}}
+@component('mail::message')
+# Hello from {{ config('app.name') }}!
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,4 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get("test", function(){
+    
+    Mail::to("giudimanuel@gmail.com")->send(new \App\Mail\TestMail());
+
+    return response()->json([
+        "message" => "Email enviado"
+    ], 200);
 });
