5ea21d23654d570e2e1c710a7f2c2b3924f02cd1
manuel <giudicimanuel@gmail.com>
2024-10-14 19:44:09 -0300

    Update Evento.php

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -75,4 +75,55 @@ class Evento extends Model
     {
         return in_array($user_id, $this->users->pluck('id')->all());
     }
+
+    function minimizarTransferencias()
+    {
+        $deudores = [];
+        $acreedores = [];
+        $saldos = [];
+
+        foreach ($this->users as $user) {
+            $saldos[$user->id] = $user->balancePorEvento($this->id);
+        }
+
+        // Separar deudores y acreedores
+        foreach ($saldos as $userId => $saldo) {
+            $usuario = User::find($userId); // Obtener el usuario por su ID
+
+            if ($saldo < 0) {
+                $deudores[] = ['usuario' => $usuario, 'monto' => -$saldo];
+            } elseif ($saldo > 0) {
+                $acreedores[] = ['usuario' => $usuario, 'monto' => $saldo];
+            }
+        }
+
+        $transferencias = [];
+
+        // Emparejar deudores con acreedores
+        $i = 0;
+        $j = 0;
+        while ($i < count($deudores) && $j < count($acreedores)) {
+            $monto = min($deudores[$i]['monto'], $acreedores[$j]['monto']);
+
+            $transferencias[] = [
+                'deudor_id' => $deudores[$i]['usuario']->id,
+                'deudor_nombre' => $deudores[$i]['usuario']->name, // Obtener el nombre del deudor
+                'acreedor_id' => $acreedores[$j]['usuario']->id,
+                'acreedor_nombre' => $acreedores[$j]['usuario']->name, // Obtener el nombre del acreedor
+                'monto' => round($monto, 2)
+            ];
+
+            $deudores[$i]['monto'] -= $monto;
+            $acreedores[$j]['monto'] -= $monto;
+
+            if ($deudores[$i]['monto'] == 0) {
+                $i++;
+            }
+            if ($acreedores[$j]['monto'] == 0) {
+                $j++;
+            }
+        }
+
+        return $transferencias;
+    }
 }
